314bc6cb7c6d5afd71e6a5be89f09db79a3c1c35
huan-yp <3051561876@qq.com>
2025-11-26 21:06:57 +0800

    ws 应该由其它服务转发

--- vite.config.js
+++ vite.config.js
@@ -16,11 +16,6 @@ export default defineConfig({
         target: 'http://localhost:3126',
         changeOrigin: true,
       },
-      '/ws': {
-        target: 'ws://localhost:3126',
-        changeOrigin: true,
-        ws: true
-      }
   }
 }
 })
